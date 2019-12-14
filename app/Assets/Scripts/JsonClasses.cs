5691c32af117d48bf0f17de46e1b6f56edc907d4
Wyzzus <wyzzus@gmail.com>
2019-12-14 18:15:56 +0300

    Add 3D Image Viewer

---
+++ app/Assets/Scripts/JsonClasses.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AppJson
+{
+    [System.Serializable]
+    public class SpinInfo
+    {
+        public Spin spin;
+    }
+    [System.Serializable]
+    public class Spin
+    {
+        public string id;
+        public string title;
+        public string caption;
+        public int frame_count;
+        public int key_frame;
+        public bool reverse;
+        public Creator creator;
+        public Account account;
+    }
+
+    public class Creator
+    {
+        public string username;
+        public string logo;
+        public object url;
+    }
+
+    public class Account
+    {
+        public Plan plan;
+    }
+
+    public class Plan
+    {
+        public bool unbranded;
+    }
+
+}
+
+
--- app/Assets/Scripts/MainManager.cs
+++ app/Assets/Scripts/MainManager.cs
@@ -2,73 +2,171 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using AppJson;
+using UnityEngine.Networking;
 
 public class MainManager : MonoBehaviour
 {
     public Transform Model;
-    public RectTransform canvas;
+    public GameObject canvas;
 
-    public Renderer ModelMarker;
     public Renderer VideoMarker;
-    public GameObject VideoPlayer;
-    public RawImage VideoPanel;
-    public Slider Rotating;
-    public Slider Scaling;
+    public RawImage Viewer;
 
-    public Vector2 ScreenSizel;
+    public GameObject Downloader;
+    public GameObject View;
 
-    public AudioSource Audio;
+    public Slider Rotator;
+    public Slider ProgressBar;
+    public bool ImageFound = false;
+
+    public string ID = "dr9zr08t3olv";
+    public string JsonUrl;
+
+    public string ImageTemplateUrl = "http://spins0.arqspin.com/";
+    public SpinInfo Info;
+
+    public Texture[] Textures;
+    public int Code = 0;
+
+    public float progress = 0;
+    public bool Debug = true;
+
+    public void CreateJsonUrl()
+    {
+        JsonUrl = "http://spins0.arqspin.com/" + ID + "/spin.json";
+    }
 
     public void Start()
     {
-
+        CreateJsonUrl();
     }
 
     public void Update()
     {
-        ScreenSizel = new Vector2(Screen.width, Screen.height);
         if(VideoMarker.enabled)
         {
-            VideoPlayer.SetActive(true);
+            ImageFound = true;
+            DownloadImages();
+        }
+
+        if(ImageFound)
+        {
+            canvas.SetActive(true);
+            switch (Code)
+            {
+                default:
+                case 0: //Default
+                    Downloader.SetActive(false);
+                    View.SetActive(false);
+                    break;
+                case 1: //Start Connection Process
+                    Downloader.SetActive(false);
+                    View.SetActive(false);
+                    break;
+                case 2: //Error
+                    Downloader.SetActive(false);
+                    View.SetActive(false);
+                    break;
+                case 3: //Start Dowload Process
+                    Downloader.SetActive(true);
+                    ProgressBar.value = progress;
+                    View.SetActive(false);
+                    break;
+                case 4: //Success
+                    Downloader.SetActive(false);
+                    ProcessImages();
+                    View.SetActive(true);
+                    break;
+            }
         }
         else
         {
-            VideoPlayer.SetActive(false);
-            if (ModelMarker.enabled)
+            canvas.SetActive(false);
+        }
+
+        
+    }
+
+    public void OpenClose()
+    {
+        ImageFound = !ImageFound;
+        canvas.SetActive(false);
+        Reset();
+    }
+
+    public void Reset()
+    {
+        StopAllCoroutines();
+        Textures = null;
+        Code = 0;
+        Viewer.texture = null;
+    }
+
+    public void ProcessImages()
+    {
+        if(Textures[(int)Rotator.value] != null)
+        {
+            Viewer.texture = Textures[(int)Rotator.value];
+        }
+    }
+
+    public void DownloadImages()
+    {
+        if(Code == 0)
+        {
+            StartCoroutine(Images());
+        }
+    }
+
+    public IEnumerator Images()
+    {
+        Code = 1;
+        using(UnityWebRequest webRequest = UnityWebRequest.Get(JsonUrl))
+        {
+            yield return webRequest.SendWebRequest();
+            Info = JsonUtility.FromJson<SpinInfo>(webRequest.downloadHandler.text);
+        }
+
+        int ImageCount = Info.spin.frame_count;
+        Textures = new Texture[ImageCount];
+
+        for(int i = 1; i <= ImageCount; i++)
+        {
+            progress = i / (float)ImageCount;
+            string number = formatNumber(i);
+            string imageUrl = ImageTemplateUrl + ID.ToString() + "/380.380/" + number + ".jpg";
+            using (UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(imageUrl))
             {
-                ModelRotating();
-                ModelScaling();
-                if (!Audio.isPlaying)
+                yield return webRequest.SendWebRequest();
+                if (webRequest.isNetworkError || webRequest.isHttpError)
                 {
-                    Audio.Play();
+                    Code = 2;
+                }
+                else
+                {
+                    Textures[i - 1] = DownloadHandlerTexture.GetContent(webRequest);
+                    Code = 3;
                 }
             }
-            else
-            {
-                Audio.Stop();
-            }
         }
-        RescaleVideo();
+        Rotator.maxValue = Textures.Length - 1;
+        Code = 4;
     }
 
-    public void ModelRotating()
+    public string formatNumber(int n)
     {
-        Model.rotation = Quaternion.Euler(0, Rotating.value, 0);
-    }
-
-    public void ModelScaling()
-    {
-        Model.localScale = Vector3.one * Scaling.value;
-    }
-
-    public void RescaleVideo()
-    {
-        if (Screen.height > Screen.width)
-            VideoPanel.rectTransform.sizeDelta = Vector2.one * canvas.sizeDelta.x;
+        if (n < 10)
+        {
+            return "00" + n.ToString();
+        }
+        else if(n < 100)
+        {
+            return "0" + n.ToString();
+        }
         else
-            VideoPanel.rectTransform.sizeDelta = Vector2.one * canvas.sizeDelta.y;
-        Debug.Log(VideoPanel.rectTransform.sizeDelta);
+        {
+            return n.ToString();
+        }
     }
-
-
 }
